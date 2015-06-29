040a565efbbebe16e833ebbf7184782ec9c61476
Dan <cobbdb@gmail.com>
2015-06-29 00:09:15 -0700

    .

--- src/sprites/buttons/open-gear.js
+++ src/sprites/buttons/open-gear.js
@@ -1,26 +1,28 @@
-var $ = require('dragonjs');
+var $ = require('dragonjs'),
+    upimg = $.pipeline.add.image('buttons/gear.png'),
+    downimg = $.pipeline.add.image('buttons/gear.down.png');
 
-module.exports = $.ui.Button({
-    name: 'open-gear',
-    pos: $.Point(0, 0),
-    size: $.Dimension(
-        $.canvas.width * 0.1,
-        $.canvas.height * 0.32
-    ),
-    up: {
-        src: 'buttons/gear.png',
-        size: $.Dimension(11, 35)
-    },
-    down: {
-        src: 'buttons/gear.down.png',
-        size: $.Dimension(11, 35)
-    },
-    onpress: function () {
-        $.screen('train').stop();
-        $.screen('care').stop();
-        $.screen('gear').start();
-        this.pause();
-        require('./open-train.js').start();
-        require('./open-care.js').start();
-    }
-});
+module.exports = function () {
+    return $.ui.Button({
+        name: 'open-gear',
+        pos: $.Point(0, 0),
+        size: $.Dimension(
+            $.canvas.width * 0.1,
+            $.canvas.height * 0.32
+        ),
+        up: {
+            image: upimg
+        },
+        down: {
+            iamge: downimg
+        },
+        onpress: function () {
+            $.screen('train').stop();
+            $.screen('care').stop();
+            $.screen('gear').start();
+            this.pause();
+            require('./open-train.js').start();
+            require('./open-care.js').start();
+        }
+    });
+};
